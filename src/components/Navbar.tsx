904b9730c9e8683e596129ad9d4b327078b6d220
fabio.secci <fabiosecci@gmail.com>
2026-04-13 21:42:30 +0200

    chore: re-trigger deploy for navigation fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+/* Navigation Ritual: Orchestration of discovery pathways */
+
 import { useState, useEffect } from "react";
 import { AnimatePresence, motion, useScroll, useMotionValueEvent } from "framer-motion";
 import Link from "next/link";
